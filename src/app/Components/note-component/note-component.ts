e9213cc29d9aa82af1e5cb9747cca2e1d1351a61
vitalischumarov <v.schumarov@icloud.com>
2026-04-23 22:35:03 +0200

    created two components templates

---
+++ src/app/Components/note-component/note-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-component',
+  imports: [],
+  templateUrl: './note-component.html',
+  styleUrl: './note-component.css',
+})
+export class NoteComponent {}
