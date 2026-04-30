903cd1c9c423ab4e97b8652bb724b148ae0c3206
vitalischumarov <v.schumarov@icloud.com>
2026-04-30 20:58:38 +0200

    button component created

--- src/app/Components/button-component/button-component.ts
+++ src/app/Components/button-component/button-component.ts
@@ -1,13 +1,13 @@
 import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-button-component',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button-component.html',
   styleUrl: './button-component.css',
 })
 export class ButtonComponent {
-
   buttonTitle = input<string>('Button Title');
-
+  buttonColor = input<string>('default');
 }
